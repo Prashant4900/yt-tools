0a95e6d5c4734ff1f51915ddcf8712503a424ad0
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2022-11-27 12:28:38 +0530

    Replace Materlize CSS to Bootstrap CSS

--- home/views.py
+++ home/views.py
@@ -7,13 +7,13 @@ def index(request):
     context = {}
     if request.method == 'POST':
         form = YouTubeForm(request.POST)
-        print("POST", form)
+        # print("POST", form)
         if form.is_valid():
             context['form'] = form
     else:
         form = YouTubeForm()
-        print("GET", form)
+        # print("GET", form)
     context['form'] = form
-    print("context", context['form'])
+    # print("context", context['form'])
     return render(request, 'base.html', context)
     # return render(request, 'base.html')
